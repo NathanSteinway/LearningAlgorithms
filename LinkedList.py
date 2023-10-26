1b9596868896ccc0fa981332207bf20df56a7d56
Nathan Steinway <steinway.nathan@gmail.com>
2023-10-26 17:59:25 -0500

    initial notes for LinkedList

---
+++ LinkedList.py
@@ -0,0 +1,30 @@
+# Python List        
+
+#  _________
+# |11|7|23|3|
+# | 0|1| 2|3|
+# -----------
+
+# Linked List
+'__________________'
+'|7 |    |    | 3  |'
+'|  |    | 23 |    |'
+'|  | 11 |    |    |'
+'|__|____|____|____|'
+
+# Key differences
+
+# Python Lists:                    |    Linked Lists:
+# 1) Contiguous, can be indexed    |    1) No index
+# 2) next to one another in mem    |    2) separate in mem
+
+# Key Features of Linked Lists
+# 1) Head points to first node in list
+# 2) Tail points to last node in list
+# 3) each node points to the next
+# 4) last node points to None
+
+
+#  HEAD            TAIL       
+#   |               |
+#   0 -> 1 -> 2 -> None
